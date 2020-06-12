a871275d501fde3a2fa90c8a79374cf65ac57f31
Sebas <i424614@fhict.nl>
2020-06-12 20:23:19 +0200

    Added migration for admin roles

--- web3LaravelProject/database/seeds/DatabaseSeeder.php
+++ web3LaravelProject/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            MessageSeeder::class, UserSeeder::class
+            messagesSeed::class, UserSeeder::class
         ]);
     }
 }
